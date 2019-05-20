293d3865875aba6ab1510001aebd0f3c4e38aa42
Seif-Miehiar <Saif.muhiar97@gmail.com>
2019-05-21 02:38:40 +0300

    New Start

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Homepage from './components/Homepage';
+
+class App extends Component {
+  state = {
+    
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Homepage />
+      </div>
+    );
+  }
+
+}
+
+export default App;
