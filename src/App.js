9840aa3dc4efececd8977d168570f286dbb2ea2f
Yazan <yazannjjar@gmail.com>
2019-05-22 11:54:02 +0300

    FirstUpdate

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { Component } from 'react';
 import $ from 'jquery'; 
 import Homepage from './components/Homepage';
@@ -39,6 +40,25 @@ class App extends Component {
             
            
           </ul>
+=======
+import React from 'react';
+// import Homepage from './components/Homepage';
+import Home from "./components/Home.js";
+
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <Home />
+>>>>>>> 09e28faa389aef5c026da2ea9fe632cf1ca7e864
       </div>
           </nav>
             <hr/>
